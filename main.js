7e707aff8a85116f2fb4608b62a0d92da2e73cff
Aaqiel <abaaqielbehardien@gmail.com>
2020-09-01 11:29:18 +0200

    created func to create img for icon | made changes to func getDates where we getting time as well and added another method to fetch the date

--- main.js
+++ main.js
@@ -8,27 +8,48 @@ const getDate = date => {
 	// mapping through weather info
 	date.map(prop => {
 		// targetting the date property of the data
-		if (prop.dt_txt) {
+		try {
 			// targetting only date within the string
-			let strDate = prop.dt_txt;
-			let currDate = strDate.substring(0, 10);
-			console.log(currDate);
+			let currDate = prop.dt_txt.substring(0, 10);
+			// let currDate = new Date(prop.dt);
+			console.log(currDate); // 2020-01-27
+			let currTime = prop.dt_txt.substing(11, 19);
+			console.log(currTime);
+			dateElement = document.createElement('span');
+			dateElement.id = 'curr-date';
+			dateElement.textContent = currDate;
+			document.querySelector('.widget').appendChild(dateElement);
+		} catch (error) {
+			alert('Cannot get dates');
 		}
 	});
+};
+
+const displayIcon = data => {
+	// Pass icon code into openweather api
+	try {
+		let locateIcon = document.querySelector('.weather-icon');
+		// Get icon code from data
+		const { icon } = data.list[0].weather[0];
+		// insert into api
+		// fetch it
+		// then create a new img
+		// display icon
+		locateIcon.innerHTML = `<img src='http://openweathermap.org/img/w/${icon}.png' />`;
+	} catch (error) {
+		alert('Not displaying icon');
+	}
 };
 
 const getCurrWeather = data => {
 	// manipulating the data that's being passed
 	if (data) {
-		try {
-			getDate(data.list[0]);
-		} catch (error) {
-			alert('Something went wrong when passing data');
-		}
+		getDate(data.list);
+		displayIcon(data);
 	} else {
 		alert('Cannot get current weather info');
 	}
 };
 
-let data = getLocalData();
-getCurrWeather(data);
+let local_data = getLocalData();
+getCurrWeather(local_data);
