906b940b1fec9c850148cbd60d58af339af4e372
Aaqiel <abaaqielbehardien@gmail.com>
2020-09-03 13:53:24 +0200

    added func display unduped list and func to get array of dates

--- main.js
+++ main.js
@@ -9,20 +9,53 @@ const getDate = date => {
 	date.map(prop => {
 		// targetting the date property of the data
 		try {
+			let DateAndTime = prop.dt_txt;
 			// targetting only date within the string
-			let currDate = prop.dt_txt.substring(0, 10);
+			let currDate = DateAndTime.slice(0, 10);
 			// let currDate = new Date(prop.dt);
 			console.log(currDate); // 2020-01-27
-			// let currTime = prop.dt_txt.substing(11, 19);
-			// console.log(currTime);
+			let currTime = DateAndTime.slice(11, 19);
+			console.log(currTime);
 			dateElement = document.createElement('span');
 			dateElement.id = 'curr-date';
 			dateElement.textContent = currDate;
 			document.querySelector('.widget').appendChild(dateElement);
 		} catch (error) {
-			alert('Cannot get dates');
+			alert(error, 'Cannot get dates');
 		}
 	});
+};
+
+const getWeekDates = data => {
+	let arr_dates = [];
+	try {
+		data.forEach(prop => {
+			// JSON dir --> data.[list].prop.value
+			// pulling date from string dt_txt
+			arr_dates.push(prop.dt_txt.slice(0, 10));
+		});
+
+		displayWeekDates(arr_dates);
+	} catch (error) {
+		alert(error, 'Cannot create list');
+	}
+};
+
+const displayWeekDates = arr => {
+	// Creating list of all dates
+	let locate_List = document.querySelector('.week-list');
+	let str_list = '<ul>';
+	// removing duplicates
+	const arr_no_dupe_dates = new Set(arr);
+
+	// convert set to array and remove first date from array
+	[...arr_no_dupe_dates].slice(1, 6).forEach(date => {
+		// display list of dates
+		str_list += '<li>' + date + '</li>';
+	});
+	str_list += '</ul>';
+
+	locate_List.innerHTML = str_list;
 };
 
 const displayIcon = data => {
@@ -37,14 +70,15 @@ const displayIcon = data => {
 		// display icon
 		locateIcon.innerHTML = `<img src='http://openweathermap.org/img/w/${icon}.png' />`;
 	} catch (error) {
-		alert('Not displaying icon');
+		alert(error, 'Not displaying icon');
 	}
 };
 
 const getCurrWeather = data => {
 	// manipulating the data that's being passed
 	if (data) {
-		getDate(data.list);
+		// getDate(data.list);
+		getWeekDates(data.list);
 		displayIcon(data);
 	} else {
 		alert('Cannot get current weather info');
